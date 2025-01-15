80453964eaf1f68f436b1bf8142496fd1e0f0d65
Ilyes.dragovic <mingo.huncho@gmail.com>
2025-01-16 00:18:12 +0100

    setting the react router

--- movieapp/src/App.jsx
+++ movieapp/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './components/Header'
 import './App.css'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import { useState } from 'react'
 function App() {
 
   return (
